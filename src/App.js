8393899600472414a6ba29b8ab2d07a273feb6a2
maxly20 <maxly2011@gmail.com>
2021-04-23 10:01:06 +1000

    building header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Header from './components/header_footer/Header';
 
 const App = () => {
-  return <div className='App'>hello</div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
