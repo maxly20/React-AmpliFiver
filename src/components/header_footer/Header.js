e134324f995353adccd89845236ee0a499629091
maxly20 <maxly2011@gmail.com>
2021-04-23 10:02:11 +1000

    building header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
     >
       <Toolbar>
         <div className='header_logo'>
-          <div className='font_righteous header_logo_venue'>AmpliFiver</div>
+          <div className='font_righteous header_logo_venue'>The OASIS</div>
           <div className='header_logo_title'>Musical Events</div>
         </div>
 
